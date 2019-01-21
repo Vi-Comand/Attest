9869a8673796278ceff432db8290d3ffdfbc21ce
kravchenko_v_e <kravcenko.vi@live.com>
2019-01-21 10:43:52 +0300

    Первая мастер страница

---
+++ Attest/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Attest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Attest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Lk()
+        {
+            return View("Lk");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+
+}
