f7b3a1e3094b3e91717a4665614c56c17bee11c3
danil <crysis12344321@gmail.com>
2019-05-31 09:35:16 +0300

    1

--- Attest/Controllers/LkController.cs
+++ Attest/Controllers/LkController.cs
@@ -81,6 +81,61 @@ namespace Attest.Controllers
 
                 return View("otv",ViewBag.users);
             }
+
+
+
+
+            if (user.role == "3")
+            {
+
+                var comments =
+                from n in db.Zayavlen.Where(p => p.mo == user.mo)
+                join f in db.Users
+                on n.id_user
+                equals f.Id
+                select new { Zayavlen = n, user = f };
+                ViewBag.users = comments;
+
+
+                /*var compositeModel = new CompositeModel(db);
+                compositeModel.Zayavlen = new Zayavlen();
+               /* compositeModel.FileModel = new FileModel();
+                compositeModel.Obrazovan = new Obrazovan();
+                compositeModel.Nauch_Deyat = new Nauch_deyat();
+                compositeModel.Users = new Users();
+                compositeModel.ProfRazv = new ProfRazv();*/
+
+                /*    compositeModel.listFile = db.File.Where(p => p.id_zayavl == id).ToList();
+                    compositeModel.listObrazovan = db.Obrazovan.Where(p => p.id_zayavl == id).ToList();
+                    compositeModel.listNauch_deyat = db.Naucn_deyat.Where(p => p.id_zayavl == id).ToList();
+                    compositeModel.listProfRazv = db.ProfRazv.Where(p => p.id_zayav == id).ToList();*/
+                /* compositeModel.ListZayavlen = db.Zayavlen.Where(p => p.mo ==user.mo).ToList();
+                 compositeModel.ListUsers = db.Users.Join(db.Zayavlen,j=);*/
+
+
+                var listotv = new ListOtv();
+                listotv.ListOtvetstven = (from zaya in db.Zayavlen.Where(p => p.spec == user.Id && p.status == "Заявление подлинное")
+                                          join usr in db.Users on zaya.id_user equals usr.Id
+                                          select new Otvetstven
+                                          {
+                                              Id = zaya.Id,
+                                              name = usr.FIO,
+                                              data_podachi = zaya.data_podachi,
+                                              oo = zaya.oo,
+                                              dolgnost = zaya.dolgnost_att,
+                                              kategor = zaya.kategor,
+                                              ball = zaya.ball
+                                          }).ToList(); 
+                return View("spec", listotv);
+
+
+
+
+                return View("otv", ViewBag.users);
+            }
+
+
+
             return View("index");
             /* ViewBag.LK = db.Zayavlen.Where(p=>p.mo==1).ToList();
            return View("otv");*/
